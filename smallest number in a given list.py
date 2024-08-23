0633b3f42f4ab15774948de3eec89396ee49b1ec
vivek06062005 <vivekchowdaryt@gmail.com>
2024-08-23 08:56:42 +0530

    Add files via upload

---
+++ smallest number in a given list.py
@@ -0,0 +1,5 @@
+def find_smallest_number(input_list):
+    return min(input_list)
+numbers = [10, 5, 8, 3, 15, 7]
+smallest_num = find_smallest_number(numbers)
+print(f"The smallest number in the list is: {smallest_num}")
