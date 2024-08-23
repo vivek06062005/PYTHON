2ec14aa75f93930e1542bc5af1509629783af812
vivek06062005 <vivekchowdaryt@gmail.com>
2024-08-23 08:56:24 +0530

    Add files via upload

---
+++ smallest number in three variables.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if a<b and a<c:
+    small=(a)
+if b<c:
+  small=(b)
+else:
+    small=(c)
+print(small)
