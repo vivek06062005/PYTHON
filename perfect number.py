9ee7cbc60c47cedd8b6433945cb5913db7e04ac0
vivek06062005 <vivekchowdaryt@gmail.com>
2024-08-23 09:09:17 +0530

    Add files via upload

---
+++ perfect number.py
@@ -0,0 +1,5 @@
+ch = input("enter the letter:")
+if ( ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='0' or ch=='u' or ch=='u'):
+   print("it is a vowel",ch)
+else:
+    print("it is a consonant",ch)
